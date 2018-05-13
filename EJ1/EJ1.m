274aff7ccda1c2ce66fd650671e865d3bf9bf03b
ffsuarez <fmsuarez92@gmail.com>
2018-05-13 18:27:09 -0300

    anadido EJ1 muestra magnitud y fase centralizada

---
+++ EJ1/EJ1.m
@@ -0,0 +1,23 @@
+%%
+% 
+%  PRACTICO 4
+% 
+%Nombre: Suarez Facundo
+clc,clear all,close all,
+%% 
+%EJERCICIO 1
+ i=imread('1.jpg');
+ i=rgb2gray(i);
+ I=fft2(i);
+ IC=fftshift(I);
+ 
+ 
+ 
+ m_IC=abs(IC);
+ log_m_IC=log(1+m_IC);
+ a_I=fftshift(angle(I));
+ 
+ 
+ 
+ figure,subplot(1,2,1),mesh(log_m_IC),title('Transform Fourier Centralizada'),
+ subplot(1,2,2),mesh(a_I),title('Fase Transf Fourier Centralizada');
