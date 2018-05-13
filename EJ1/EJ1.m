a37ef640b86f8b1287ba58960c6af8dcabcea8d7
ffsuarez <fmsuarez92@gmail.com>
2018-05-13 18:43:39 -0300

    modificacion: Para responder EJ1 a b c

--- EJ1/EJ1.m
+++ EJ1/EJ1.m
@@ -11,16 +11,21 @@ clc,clear all,close all,
  I=fft2(i);
  IC=fftshift(I);
  
- 
+ %a
+ figure,subplot(1,3,1),imshow(i),title('Imagen original'),
+ subplot(1,3,2),imshow(I),title('Transformada No Centralizada'),
+ subplot(1,3,3),imshow(IC),title('Transformada Centralizada');
  
  m_IC=abs(IC);
  log_m_IC=log(1+m_IC);
  a_I=fftshift(angle(I));
  
- 
- 
+  
+ %b
  figure,subplot(1,2,1),mesh(log_m_IC),title('Transform Fourier Centralizada'),
  subplot(1,2,2),mesh(a_I),title('Fase Transf Fourier Centralizada');
  
  figure,subplot(1,2,1),mesh(log(1+abs(I))),title('Transf Fourier No Centralizada');
  subplot(1,2,2),mesh(angle(I)),title('Ang Transf Fourier No Centralizada')
+ 
+ 
